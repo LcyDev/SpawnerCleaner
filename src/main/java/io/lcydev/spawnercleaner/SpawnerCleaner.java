a34f7fafdb34dbd2a6fa6c279c80e589fd1346f3
Lucyy_ <conahiromi@gmail.com>
2023-01-23 21:10:08 -0300

    Initial Commit

---
+++ src/main/java/io/lcydev/spawnercleaner/SpawnerCleaner.java
@@ -0,0 +1,15 @@
+package io.lcydev.spawnercleaner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnerCleaner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
