2b307ac141f89a8ecaec44390e17adb9224aa491
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-07-19 21:20:56 -0300

    Create ChunkWrapper.kt

---
+++ src/main/kotlin/io/lcydev/spawnercleaner/handlers/ChunkWrapper.kt
@@ -0,0 +1,14 @@
+package io.lcydev.template.handlers
+
+
+class ChunkWrapper {
+    private val worldName: String? = null
+
+    private val x = 0
+
+    private val z = 0
+
+    private val coords: IntArray = TODO()
+
+    //private val state: ChunkState = ChunkState.UNPROCESSED
+}
